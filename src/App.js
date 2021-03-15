f7b82d00db2cc05a118239d30c7e78319a3e7e7b
Jeff Goin <goin.jeffrey@gmail.com>
2021-03-14 17:13:59 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx'
-import {Switch, Route} from 'react-router-dom';
+import Header from './components/header/header.component.jsx'
 
 const HatsPage = () => (
   <div>
@@ -14,6 +17,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path = '/' component={HomePage} />
         <Route path= '/shop' component={ShopPage}/>
